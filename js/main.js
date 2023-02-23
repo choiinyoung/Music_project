740b3eac4738f8eb855049707db374f2c794c968
choiinyoung <choi121100@naver.com>
2023-02-23 14:27:06 +0900

    musin 추가 및 중복체크

--- js/main.js
+++ js/main.js
@@ -11,9 +11,19 @@ const names = ["cardio","groove","happy","light","lily","limes","pop","swing"];
 for(let i = 0;i<len; i++){
   articleArr[i].style.transform = `rotate(${deg * i}deg) translateY(-100vh)`
   
-  //사진 부분 일괄 적용
+  // 사진 부분 일괄 적용
   const pic = articleArr[i].querySelector(".pic");
   pic.style.backgroundImage = `url("../img/${names[i]}.jpg")`;
+
+  // 음악 제목 일괄 적용
+  const title = articleArr[i].querySelector(".text>h2");
+  title.innerHTML = `${names[i]}`;
+
+  // 음악 태그 & 파일 일괄 적용
+  const audio = document.createElement("audio");
+  audio.setAttribute("src",`../music/${names[i]}.mp3`);
+  audio.setAttribute("loop","loop");
+  articleArr[i].append(audio);
 }
 
 // Prev Next 버튼 액션 처리
@@ -52,3 +62,43 @@ next.addEventListener("click", function () {
   articleArr[active].classList.add("on")
 });
 
+// CD 모양 사진 회전 & 음악 파일 컨트롤
+let before = 0;
+
+for(let el of articleArr) {
+  const play = el.querySelector(".play");
+  const pause = el.querySelector(".pasue");
+  const reload = el.querySelector(".reload");
+
+  play.addEventListener("click", function (e){
+    //el.querySelector(".pic").classList.add("on");
+    if(before === 0){
+      before = e.currentTarget;
+    } else if (before !== e.currentTarget) {
+      before.closest("article").querySelector("audio").pause();
+      before.closest("article").querySelector(".pic").classList.remove("on");
+      before = e.currentTarget;
+    }
+    e.target.closest("article").querySelector(".pic").classList.add("on");
+    el.querySelector("audio").play();
+
+  });
+  pause.addEventListener("click", function (e){
+    //el.querySelector(".pic").classList.remove("on");
+    e.target.closest("article").querySelector(".pic").classList.remove("on");
+    e.target.closest("article").querySelector("audio").pause();
+  });
+  reload.addEventListener("click",function(){
+    if(before === 0){
+      before = e.currentTarget;
+    } else if (before !== e.currentTarget) {
+      before.closest("article").querySelector("audio").pause();
+      before.closest("article").querySelector(".pic").classList.remove("on");
+      before = e.currentTarget;
+    }
+    
+    el.querySelector(".pic").classList.add("on");
+    el.querySelector("audio").load();
+    el.querySelector("audio").play();
+  })
+}
